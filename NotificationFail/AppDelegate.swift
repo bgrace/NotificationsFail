438d7c909c42ed2f20cc00409eea8ac0f3aca5f2
Brett Grace <bgrace@mac.com>
2018-04-26 08:46:33 -0700

    Initial Commit

---
+++ NotificationFail/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NotificationFail
+//
+//  Created by Brett Grace on 4/26/18.
+//  Copyright © 2018 Brett Grace. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NotificationFail/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NotificationFail
+//
+//  Created by Brett Grace on 4/26/18.
+//  Copyright © 2018 Brett Grace. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
