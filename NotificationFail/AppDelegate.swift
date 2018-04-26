6c1c6501486d2bcb7a45cfe4a4650416de1da698
Brett Grace <bgrace@mac.com>
2018-04-26 10:32:33 -0700

    Initial commit

--- NotificationFail/AppDelegate.swift
+++ NotificationFail/AppDelegate.swift
@@ -9,18 +9,47 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
 
-
+    var vc: ViewController?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        NSUserNotificationCenter.default.delegate = self
+        if let mainWindow = NSApp.mainWindow,
+            let vc = mainWindow.contentViewController as? ViewController {
+            self.vc = vc
+        }
+        
+        vc?.append(text: "Contents of notification.userInfo:\n\(aNotification.userInfo?.debugDescription ?? "THAT'S IMPOSSIBLE")")
+        
+        if let _ = aNotification.userInfo?[NSApplication.launchUserNotificationUserInfoKey] as? NSUserNotification {
+            vc?.foundNotificationCheckbox.state = .on
+        } else {
+            vc?.foundNotificationCheckbox.state = .off
+        }
+
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
-
+    
+    func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
+        return true
+    }
+    
+    func userNotificationCenter(_ center: NSUserNotificationCenter, didDeliver notification: NSUserNotification) {
+        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 1) {
+            NSApp.terminate(self)
+        }
+    }
+    
+    func userNotificationCenter(_ center: NSUserNotificationCenter, didActivate notification: NSUserNotification) {
+        // Should never reach this because we close automatically, just added to rule out its presence/absence as contributing
+        vc?.append(text: "Handling an action from NSUserNotification:\n\(notification.debugDescription)")
+    }
+    
 
 }
 
--- NotificationFail/ViewController.swift
+++ NotificationFail/ViewController.swift
@@ -10,18 +10,51 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var withAdditionalActionButton: NSButton!
+    @IBOutlet weak var withoutAdditionalActionButton: NSButton!
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var foundNotificationCheckbox: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    func append(text: String) {
+        textView.textStorage?.append(NSAttributedString(string: "\(text)\n----------------\n"))
+    }
+    
+    func buildNotification(title: String) -> NSUserNotification {
+        let identifier = UUID().uuidString
+        let notification = NSUserNotification()
+        notification.informativeText = identifier
+        notification.identifier = identifier
+        notification.title = title
+        return notification
+    }
+    
+    @IBAction func withAdditionalActionPressed(_ sender: Any) {
+        let notification = buildNotification(title: "With additional action!")
+        notification.additionalActions = [
+            NSUserNotificationAction(identifier: "one", title: "Action One"),
+            NSUserNotificationAction(identifier: "two", title: "Action Two")
+        ]
+        NSUserNotificationCenter.default.deliver(notification)
+    }
+    
+    @IBAction func withoutAdditionalActionPressed(_ sender: Any) {
+        let notification = buildNotification(title: "Without additional action!")
+        NSUserNotificationCenter.default.deliver(notification)
+
+    }
+    
+    @IBAction func checkCheckbox(_ sender: NSButton) {
+        // make it read-only
+        if sender.state == .off {
+            sender.state = .on
+        } else {
+            sender.state = .off
         }
     }
-
-
+    
 }
 
